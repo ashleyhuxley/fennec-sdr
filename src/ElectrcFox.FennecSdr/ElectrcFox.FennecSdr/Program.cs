d1d94531a949bfd4338af49b7da9060ae6b36398
Ashley Huxley <git@electricfox.co.uk>
2025-12-25 12:57:22 +0000

    New calibration values

--- src/ElectrcFox.FennecSdr/ElectrcFox.FennecSdr/Program.cs
+++ src/ElectrcFox.FennecSdr/ElectrcFox.FennecSdr/Program.cs
@@ -9,7 +9,7 @@ public class Program
     {
         Console.WriteLine("Fennec SDR Ready");
 
-        var touchCal = new TouchCalibration(474, 3352, 332, 3900, true, true, false);
+        var touchCal = new TouchCalibration(369, 3538, 332, 3900, true, false, true);
 
         var lcd = new Ili9341(0, 0);
         var touch = new Xpt2046(spiBusId: 0, csPin: 1, irqPin: 17, touchCal);
