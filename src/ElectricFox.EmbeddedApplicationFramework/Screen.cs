023e2d2854c36e7310c196289b576796dab28327
Ashley Huxley <git@electricfox.co.uk>
2026-01-01 21:50:41 +0000

    Add bg colour for screen

--- src/ElectricFox.EmbeddedApplicationFramework/Screen.cs
+++ src/ElectricFox.EmbeddedApplicationFramework/Screen.cs
@@ -1,4 +1,6 @@
-﻿using ElectricFox.EmbeddedApplicationFramework.Ui;
+﻿using ElectricFox.EmbeddedApplicationFramework.Graphics;
+using ElectricFox.EmbeddedApplicationFramework.Ui;
+using SixLabors.ImageSharp;
 
 namespace ElectricFox.EmbeddedApplicationFramework;
 
@@ -6,12 +8,20 @@ public abstract class Screen : UiContainer
 {
     protected AppHost App { get; private set; } = null!;
 
+    public Color BackgroundColor { get; set; } = Color.Black;
+
     internal void Attach(AppHost app) => App = app;
 
     public virtual void OnEnter() { }
     public virtual void OnExit() { }
 
     public virtual void Update(TimeSpan delta) { }
+
+    public override void Render(GraphicsRenderer renderer)
+    {
+        renderer.Clear(BackgroundColor);
+        base.Render(renderer);
+    }
 }
 
 public abstract class Screen<TResult> : Screen
